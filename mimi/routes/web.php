91a1506bbcdaa742c2513f0551b32e9b5762c314
mimiyu5 <santillanloriemae@gmail.com>
2025-02-25 10:56:49 +0800

    second  act-create

--- mimi/routes/web.php
+++ mimi/routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/', [BookController::class,'index']); // Display all books
+Route::get('/create', [BookController::class, 'create']); // Show form to create a book
+Route::post('/store', [BookController::class, 'store']); // Store the new book
