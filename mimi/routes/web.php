48184503db2dad50e5672d2848c7a8f5dd93beb8
mimiyu5 <santillanloriemae@gmail.com>
2025-02-25 10:59:15 +0800

    3rd act-update/delete

--- mimi/routes/web.php
+++ mimi/routes/web.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\BookController;
 Route::get('/', [BookController::class,'index']); // Display all books
 Route::get('/create', [BookController::class, 'create']); // Show form to create a book
 Route::post('/store', [BookController::class, 'store']); // Store the new book
+Route::get('/edit/{id}', [BookController::class, 'edit']); // Show form to edit a book
+Route::put('/update/{id}', [BookController::class, 'update']); // Update a book
+Route::delete('/destroy/{id}', [BookController::class, 'destroy']); // Delete a book
+  
